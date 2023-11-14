ce2573152b2b31fd84c5e87564fbf24e0fa23210
Saad Khawaja <saadnk@hotmail.co.uk>
2023-11-14 15:59:35 +0000

    adding an unittest file

--- tests/test_.py
+++ tests/test_.py
@@ -1,14 +1,14 @@
 import csv
 import unittest
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import products_and_prices_dict
-# import os
-# import sys
 
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# import products_and_prices_dict
-
-# Note: This test is prediciated on the csv file having headers already present, as listed in the self.first_row below
+#Note: This test is prediciated on the csv file having headers already present, as listed in the self.first_row below
 class testloaddata(unittest.TestCase):
     
     def setUp(self):
@@ -20,7 +20,7 @@ class testloaddata(unittest.TestCase):
                      "payment_type":"CARD",
                      "card_number":"5494173772652516"
         }
-        self.filename = '../chesterfield_25-08-2021_09-00-00.csv'
+        self.filename = "C:\\Users\\saadn\\OneDrive\\Documents\\bootcamp stuff\\oneders\\chesterfield_25-08-2021_09-00-00.csv"
         
     
     def test_loading_data(self):
@@ -32,12 +32,12 @@ class testloaddata(unittest.TestCase):
     
     def test_extracting_basket_value(self):
         all_4th_values = products_and_prices_dict.extract_4th_column(self.filename)
-        expected_outcome = {"basket":"Regular Flavoured iced latte - Hazelnut - 2.75, Large Latte - 2.45"}
-        actual_outcome= {}.append(all_4th_values[0])
+        expected_outcome = ["Regular Flavoured iced latte - Hazelnut - 2.75, Large Latte - 2.45"]
+        actual_outcome= [all_4th_values[0]]
         
         self.assertEqual(expected_outcome,actual_outcome)
         
-        
+    #to run tests, one must enter  pytest tests/test_.py in the termal from the onders directory    
         
         
         
